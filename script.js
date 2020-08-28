08454a050798f2463c20089d8dd7ef2d88657d46
awkumar <Awadhesh.Kumar@harman.com>
2020-08-28 19:49:12 +0530

    Final release

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("testing");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    // const constraints = { audio: true, video: { width: 640, height: 320 } };
+    // const mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    // Catch error
+    console.log("woops, error here", error);
+  }
+}
+
+// Event Listener
+button.addEventListener("click", async () => {
+  // Disable button
+  button.disabled = true;
+
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+
+  // Reset button
+  button.disabled = false;
+});
+
+// On load
+selectMediaStream();
